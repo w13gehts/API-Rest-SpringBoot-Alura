bbd9697b65e988348f481264a81b021e69b95d86
Juliana <juliana.salafia@gmail.com>
2021-07-03 17:19:10 -0300

    Criando uma API Rest com SpringBoot, curso da Alura - parte 1

---
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -0,0 +1,28 @@
+package br.com.alura.forum.controller;
+
+import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class TopicosController {
+
+    @Autowired
+    private TopicoRepository topicoRepository;
+
+    @RequestMapping("/topicos")
+    public List<TopicoDto> lista(String nomeCurso) {
+        if (nomeCurso == null) {
+            List<Topico> topicos = topicoRepository.findAll();
+            return TopicoDto.converter(topicos);
+        } else {
+            List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+            return TopicoDto.converter(topicos);
+        }
+    }
+}
